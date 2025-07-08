d11517cf91e462a890fa00f1a2a73194a3204880
Daniel Santiago Parra Lopez <lopezgodaniell@gmail.com>
2025-07-07 19:11:13 -0500

    feat: se agrego el hero, se mejorar los estilos del navbar

--- frontend/public/js/components/navbar.js
+++ frontend/public/js/components/navbar.js
@@ -12,7 +12,30 @@ document.addEventListener('DOMContentLoaded', function() {
             .then(response => response.text())
             .then(data => {
                 navbarElement.innerHTML = data;
-            })
-            .catch(error => console.error("Error al cargar el navbar", error));
+
+            //------Lógica para resaltar el enlace activo------
+            
+            // Obtener la URL (ruta) actual
+            // Si no hay archivo en especifico se asume que es el index
+            //  
+            const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+            //Seleccionar todos los enlaces del navbar que usaran la clase personalizada
+            const navLinks = navbarElement.querySelectorAll(".navbar__link");
+
+            // Recorrer cada enlace del navbar
+            navLinks.forEach(link => {
+
+                //------ Verificar si el href del enlace incluye el nombre de la pagina actual------
+
+                // Si link obtiene el atributo de href "(link.getAtrribute("href")" entonces, incluyalo en la pagina actual ".includes(currentPage)"
+                if (link.getAttribute("href").includes(currentPage)) {
+                    // Si es la pagina actual se le asigna la clase {"active"} para destacar visualmente
+                    link.classList.add("active");
+                }
+                
+            });
+        })
+    .catch(error => console.error("Error al cargar el navbar", error));
     }
 });
